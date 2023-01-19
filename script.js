dcb705d2ec9c4a15a1458b838f44bad1591ed278
sumit7057416306 <120789691+sumit7057416306@users.noreply.github.com>
2023-01-19 22:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,15 +9,37 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  const SYMBOLS = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    36:"XXXVI",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+};
+
+    let roman = "";
+    let keys = Object.keys(SYMBOLS).reverse();
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        while (num >= key) {
+            roman += SYMBOLS[key];
+            num -= key;
+        }
+    }
+    return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
+ console.log(convertToRoman(36));
 
 // do not edit below this line
 module.exports = convertToRoman
